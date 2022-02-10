b5246d7262d850d934850e1f4f2d94011978f3e9
nushtosh <vik.golubeva2010@gmail.com>
2022-02-11 00:35:36 +0300

    t2

---
+++ Matrix/Matrix/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+namespace Matrix
+{
+    internal class Program
+    {
+        private static readonly string FilesPath = $"{Directory.GetParent(Environment.CurrentDirectory)?.Parent?.Parent?.FullName}\\test_data";
+
+        public static void Main(string[] args)
+        {
+            System.Threading.Thread.CurrentThread.CurrentCulture = new System.Globalization.CultureInfo("en-US");
+            Console.WriteLine("state the names of matrix files: ");
+            string path1 = Console.ReadLine();
+            string path2 = Console.ReadLine();
+
+            var rw = new ReaderWriter<double>();
+            var m1 = rw.ReadFile($"{FilesPath}\\{path1}");
+            var m2 = rw.ReadFile($"{FilesPath}\\{path2}");
+
+            var tmp1 = m1.ToArray();
+            var tmp2 = m2.ToArray();
+
+            var mm1 = new Matrix<double>(tmp1);
+            var mm2 = new Matrix<double>(tmp2);
+
+            Console.WriteLine("First matrix:");
+            Console.WriteLine(mm1);
+            Console.WriteLine("Second matrix:");
+            Console.WriteLine(mm2);
+
+            var mm3 = mm1.Mul(mm2);
+
+            Console.WriteLine("Result matrix:");
+            Console.WriteLine(mm3);
+
+            rw.WriteFile(mm3.Data, $"{FilesPath}\\resMatrix.txt");
+        }
+    }
+}
---
+++ Matrix/Matrix/ReaderWriter.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Runtime;
+using System.Collections.Generic;
+
+namespace Matrix
+{
+    public class ReaderWriter<T>
+{
+    private readonly char _delimiter;
+
+
+    public ReaderWriter(char delimiter = ';')
+    {
+        _delimiter = delimiter;
+    }
+
+    public T[][] ReadFile(string filepath)
+    {
+        var res = new ResizeArray<T[]>();
+        using (var sr = File.OpenText(filepath))
+        {
+            string s;
+            while ((s = sr.ReadLine()) != null)
+            {
+                var line = s.Split(_delimiter).Select((s1 => (T)Convert.ChangeType(s1, typeof(T))));
+                    var arline = line.ToArray();
+                res.Add(arline);
+            }
+        }
+
+        return res.Value();
+    }
+
+    public void WriteFile(T[][] mx, string filepath)
+    {
+        var enumerable = mx
+            .AsEnumerable()
+            .Select(
+                line => line
+                    .AsEnumerable()
+                    .Select(num => $"{num}")
+                    .Aggregate((accum, current) => accum + _delimiter + current)
+                );
+        File.WriteAllLines(filepath, enumerable);
+    }
+}
+}
