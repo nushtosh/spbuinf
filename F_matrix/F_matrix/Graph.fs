df425cba07fcd64f3b19b645be175f1e3e5e7420
nushtosh <vik.golubeva2010@gmail.com>
2022-04-19 17:12:22 +0300

    fixed diagnostics..i think

--- F_matrix/F_matrix/Graph.fs
+++ F_matrix/F_matrix/Graph.fs
@@ -6,19 +6,12 @@ open QuickGraph.Algorithms
 open System.IO
 open System.Diagnostics
 open System.Collections.Generic
+open System
 
 
 let shortestPathDijkstra (ag: AdjacencyGraph<int, Edge<int>>) (mx: _[,]) fromVertex = 
     ag.ShortestPathsDijkstra((fun edge -> mx.[edge.Source, edge.Target]), fromVertex)
 
-let exportToPdf (pathToDot: string) (pathToOutputDir: string) = 
-   let filename = Path.GetFileNameWithoutExtension pathToDot
-   let outputFile = Path.GetFullPath <| Path.Combine [| pathToOutputDir; filename + ".pdf" |]
-   let pInfo = new ProcessStartInfo()
-   pInfo.FileName <- "dot"
-   pInfo.Arguments  <- sprintf "-Tpdf -o %s %s" outputFile pathToDot
-   use p = Process.Start(pInfo)
-   if (p.WaitForExit(10000)) then outputFile else p.ExitCode.ToString()
 
 let adjacencyGraph (mx: _[,]) =
    let len = mx.GetLength 0
@@ -41,3 +34,40 @@ let createDotFileTrc (filename: string) matrix (trc:int[,]) =
             fprintfn writer "\t%i -> %i  [color=red];" i j
     ) matrix
     fprintfn writer "}"
+
+let runProc filename args= 
+    let procStartInfo = 
+        ProcessStartInfo(
+            RedirectStandardOutput = true,
+            RedirectStandardError = true,
+            UseShellExecute = false,
+            FileName = filename,
+            Arguments = args
+        )
+
+    let outputs = System.Collections.Generic.List<string>()
+    let errors = System.Collections.Generic.List<string>()
+    let outputHandler f (_sender:obj) (args:DataReceivedEventArgs) = f args.Data
+    let p = new Process(StartInfo = procStartInfo)
+    p.OutputDataReceived.AddHandler(DataReceivedEventHandler (outputHandler outputs.Add))
+    p.ErrorDataReceived.AddHandler(DataReceivedEventHandler (outputHandler errors.Add))
+    let started = 
+        try
+            p.Start()
+        with | ex ->
+            reraise()
+    if not started then
+        failwithf "Failed to start process %s" filename
+    
+    printfn "Started %s with pid %i" p.ProcessName p.Id
+    p.BeginOutputReadLine()
+    p.BeginErrorReadLine()
+    p.WaitForExit()
+
+
+
+let exportToPdf (pathToDot: string) (pathToOutputDir: string) = 
+   let filename = Path.GetFileNameWithoutExtension pathToDot
+   let outputFile = Path.GetFullPath <| Path.Combine [| pathToOutputDir; filename + ".pdf" |]
+   runProc "dot" (sprintf "-Tpdf -o %s %s" outputFile pathToDot)
+   outputFile
